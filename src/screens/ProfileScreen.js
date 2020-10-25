53568c2a295680ece58587131dd67a2fcebeac04
PUMINAN PICROH (ภูมินันท์ พิเคราะห์) <6030213026@email.psu.ac.th>
2020-10-25 20:24:27 +0700

    component profile

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -5,28 +5,42 @@ import { UserContext } from "../context/UserContext";
 import { FirebaseContext } from "../context/FirebaseContext";
 
 import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
 
 export default ProfileScreen = () => {
   return (
     <View>
-
-      <View>
-        <View style={prop.profilepic}>
-          <ProfilePic style={component.profilepic}></ProfilePic>
-        </View>
-      </View>
-
-      <View style={prop.editpicicon}>
-        <TouchableOpacity>
-          <EditPicIcon style={component.editpicicon}></EditPicIcon>
-          <Text style={{ transform: [{ rotate: '90deg' }], fontSize: 10, position: 'relative', top: 165, right: 150 }}>edit</Text>
-        </TouchableOpacity>
-      </View>
-
-
       <View>
         <View>
+          <View style={prop.profilepic}>
+            <ProfilePic style={component.profilepic}></ProfilePic>
+          </View>
+        </View>
 
+        {/* <View style={prop.editpicicon}>
+          <TouchableOpacity>
+            <EditPicIcon style={component.editpicicon}></EditPicIcon>
+            <Text style={{ transform: [{ rotate: '90deg' }], fontSize: 10, position: 'relative', top: 165, right: 150 }}>edit</Text>
+          </TouchableOpacity>
+        </View> */}
+      </View>
+
+      <View style={prop.formprofile}>
+        <View style={component.formprofile}>
+          <Label>name</Label>
+          <TextInput></TextInput>
+          <Label>bio</Label>
+          <TextInput></TextInput>
+          <Label>telephone number</Label>
+          <TextInput></TextInput>
+          <Label>email</Label>
+          <TextInput></TextInput>
+          <Label>address</Label>
+          <TextInput></TextInput>
+          <TextInput></TextInput>
+          <TextInput></TextInput>
+          <TextInput></TextInput>
+          <TextInput></TextInput>
         </View>
       </View>
     </View>
@@ -34,7 +48,14 @@ export default ProfileScreen = () => {
 };
 
 const ProfilePic = styled.View``;
+
 const EditPicIcon = styled.View``;
+
+const Label = styled.Text`
+    flex-direction: row;
+    color: gray;
+    font-weight: bold;
+`;
 
 const component = StyleSheet.create({
   profilepic: {
@@ -51,6 +72,9 @@ const component = StyleSheet.create({
     borderColor: 'white',
     borderWidth: 2,
   },
+  formprofile: {
+    width: 270,
+  },
 })
 
 const prop = StyleSheet.create({
@@ -63,5 +87,9 @@ const prop = StyleSheet.create({
     position: 'relative',
     left: 210,
     top: -39,
+  },
+  formprofile: {
+    flexDirection: 'column',
+    alignItems: 'center',
   },
 })
