954da85c959cae871fa6a3bdbdee198562909758
PUMINAN PICROH (ภูมินันท์ พิเคราะห์) <6030213026@email.psu.ac.th>
2020-10-26 17:09:33 +0700

    incomplete profile

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,19 +1,25 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
 import styled from "styled-components";
 
 import { UserContext } from "../context/UserContext";
 import { FirebaseContext } from "../context/FirebaseContext";
 
-import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity, Alert, Modal, TouchableHighlight } from "react-native";
 import { TextInput } from "react-native-gesture-handler";
 
 export default ProfileScreen = () => {
+
+  const [modalVisible, setModalVisible] = useState(false);
+
   return (
     <View>
       <View>
         <View>
           <View style={prop.profilepic}>
-            <ProfilePic style={component.profilepic}></ProfilePic>
+            <ProfilePicฺBorder style={component.profilepicborder}>
+              <ProfilePic style={component.profilepic}>
+              </ProfilePic>
+            </ProfilePicฺBorder>
           </View>
         </View>
 
@@ -27,21 +33,58 @@ export default ProfileScreen = () => {
 
       <View style={prop.formprofile}>
         <View style={component.formprofile}>
+
           <Label>name</Label>
-          <TextInput></TextInput>
+          <TextInput style={component.textinput} value="Puminan Picroh" editable={false}></TextInput>
           <Label>bio</Label>
-          <TextInput></TextInput>
+          <TextInput style={component.textinput} value="I am new" editable={false}></TextInput>
           <Label>telephone number</Label>
-          <TextInput></TextInput>
+          <TextInput style={component.textinput} value="062-3578734" editable={false}></TextInput>
           <Label>email</Label>
-          <TextInput></TextInput>
+          <TextInput style={component.textinput} value="strompoom@gmail.com" editable={false}></TextInput>
           <Label>address</Label>
-          <TextInput></TextInput>
-          <TextInput></TextInput>
-          <TextInput></TextInput>
-          <TextInput></TextInput>
-          <TextInput></TextInput>
+          <TextInput style={component.textinput} value="80/1 Male Domitory PSU" editable={false}></TextInput>
+          <TextInput style={component.textinput} value="Kathu" editable={false}></TextInput>
+          <TextInput style={component.textinput} value="Kathu" editable={false}></TextInput>
+          <TextInput style={component.textinput} value="Phuket" editable={false}></TextInput>
         </View>
+      </View>
+
+
+
+      <View style={styles.centeredView}>
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => {
+            Alert.alert("Modal has been closed.");
+          }}
+        >
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <Text style={styles.modalText}>Hello World!</Text>
+
+              <TouchableHighlight
+                style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
+                onPress={() => {
+                  setModalVisible(!modalVisible);
+                }}
+              >
+                <Text style={styles.textStyle}>Hide Modal</Text>
+              </TouchableHighlight>
+            </View>
+          </View>
+        </Modal>
+
+        <TouchableHighlight
+          style={styles.openButton}
+          onPress={() => {
+            setModalVisible(true);
+          }}
+        >
+          <Text style={styles.textStyle}>Show Modal</Text>
+        </TouchableHighlight>
       </View>
     </View>
   );
@@ -49,19 +92,30 @@ export default ProfileScreen = () => {
 
 const ProfilePic = styled.View``;
 
-const EditPicIcon = styled.View``;
+const ProfilePicฺBorder = styled.View``;
 
 const Label = styled.Text`
     flex-direction: row;
-    color: gray;
+    color: #707070;
     font-weight: bold;
 `;
 
 const component = StyleSheet.create({
+  profilepicborder: {
+    width: 140,
+    height: 140,
+    borderRadius: 100,
+    borderWidth: 2,
+    borderStyle: 'dashed',
+    borderColor: 'gray',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   profilepic: {
     backgroundColor: 'gray',
-    width: 120,
-    height: 120,
+    width: 115,
+    height: 115,
     borderRadius: 100,
   },
   editpicicon: {
@@ -75,6 +129,9 @@ const component = StyleSheet.create({
   formprofile: {
     width: 270,
   },
+  textinput: {
+    color: 'gray',
+  },
 })
 
 const prop = StyleSheet.create({
@@ -82,6 +139,7 @@ const prop = StyleSheet.create({
     paddingTop: 25,
     flexDirection: 'row',
     justifyContent: 'center',
+    alignItems: 'center',
   },
   editpicicon: {
     position: 'relative',
@@ -89,7 +147,48 @@ const prop = StyleSheet.create({
     top: -39,
   },
   formprofile: {
+    paddingTop: 20,
     flexDirection: 'column',
     alignItems: 'center',
   },
 })
+
+// modal
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5
+  },
+  openButton: {
+    backgroundColor: "#F194FF",
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
+  }
+});
