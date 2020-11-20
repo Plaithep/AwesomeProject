d25a08ad046b5c874c58555809373fa67b6054ee
Bhumin <48542992+dhinvhum@users.noreply.github.com>
2020-11-20 23:58:56 +0700

    chats

--- src/screens/Tab/MessageScreen.js
+++ src/screens/Tab/MessageScreen.js
@@ -1,5 +1,5 @@
-import React, { Component,useContext,useState } from "react";
-import { StyleSheet, TextInput, View, Text } from "react-native";
+import React, { Component, useContext, useState } from "react";
+import { StyleSheet, TextInput, View,   TouchableOpacity, Text } from "react-native";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import styled from "styled-components";
 import { FirebaseContext } from "../../context/FirebaseContext";
@@ -10,17 +10,38 @@ export default MessageScreen = ({ navigation }) => {
 
   return (
     <KeyboardAwareScrollView>
-      <Container>
-
-      </Container>
+      <View style={prop.container}>
+        <TouchableOpacity style={prop.chats}>
+          <View style={prop.profilepic}></View>
+          <Text>Name Surname</Text>
+        </TouchableOpacity>
+      </View>
     </KeyboardAwareScrollView>
 
   );
 
 };
 
-const Container = styled.View`
-    align-items: center;
-    margin-top: 64px;
-    flex: 1;
-`;
+const prop = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    margin: '5%',
+  },
+  chats: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '100%',
+    backgroundColor: "white",
+    borderRadius: 10,
+    padding: 10
+  },
+  profilepic: {
+    width: 60,
+    height: 60,
+    backgroundColor: 'gray',
+    borderRadius: 60,
+    marginLeft: 10,
+    marginRight: 20,
+  }
+})
