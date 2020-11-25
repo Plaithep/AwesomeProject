d526a6403948f6c6eb87279897940812514a85cb
Bhumin <48542992+dhinvhum@users.noreply.github.com>
2020-11-25 23:38:00 +0700

    message complete ♥

--- src/screens/Tab/MessageScreen.js
+++ src/screens/Tab/MessageScreen.js
@@ -1,29 +1,33 @@
 import React, { Component, useContext, useState } from "react";
-import { StyleSheet, TextInput, View,   TouchableOpacity, Text } from "react-native";
+import { StyleSheet, TextInput, View, TouchableOpacity, Text, ImageBackground } from "react-native";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import styled from "styled-components";
 import { FirebaseContext } from "../../context/FirebaseContext";
 import { firestore } from "firebase";
+import { ImagePropTypes } from "react-native";
 
 
 export default MessageScreen = ({ navigation }) => {
 
-  return (
-    <KeyboardAwareScrollView>
-      <View style={prop.container}>
-        {/* chat 1 */}
-        <TouchableOpacity style={prop.chats}>
-          <View style={prop.profilepic}></View>
-          <Text>Name Surname</Text>
-        </TouchableOpacity>
-        {/* chat 2 */}
-        <TouchableOpacity style={prop.chats}>
-          <View style={prop.profilepic}></View>
-          <Text>Name Surname</Text>
-        </TouchableOpacity>
-      </View>
-    </KeyboardAwareScrollView>
+  const imagebackground = { uri: "https://i.pinimg.com/564x/36/ae/1c/36ae1c8441c61dc2e6268f8077f0dd19.jpg" };
 
+  return (
+    <ImageBackground source={imagebackground} style={prop.image}>
+      <KeyboardAwareScrollView>
+        <View style={prop.container}>
+          {/* chat 1 */}
+          <TouchableOpacity style={prop.chats}>
+            <View style={prop.profilepic}></View>
+            <Text>Name Surname</Text>
+          </TouchableOpacity>
+          {/* chat 2 */}
+          <TouchableOpacity style={prop.chats}>
+            <View style={prop.profilepic}></View>
+            <Text>Name Surname</Text>
+          </TouchableOpacity>
+        </View>
+      </KeyboardAwareScrollView>
+    </ImageBackground>
   );
 
 };
@@ -50,5 +54,10 @@ const prop = StyleSheet.create({
     borderRadius: 60,
     marginLeft: 10,
     marginRight: 20,
-  }
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+  },
 })
